0b188d8527f099996630a4f61ca9e6c336325067
mirnijam <designer.nijamuddin@gmail.com>
2025-01-11 14:31:19 -0800

    commit

---
+++ assets/componets/header.php
@@ -0,0 +1,24 @@
+<nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+      <div class="search-box">
+        <input type="text" placeholder="Search...">
+        <button><i class='bx bx-search' ></i></button> 
+      </div>
+      <div class="dropdown profile">
+        <button class="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <div class="profile-details">
+                <img src="img/01.webp" alt="">
+                <span class="admin_name">Prem Shahi</span>
+                <i class='bx bx-chevron-down' ></i>
+              </div>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="myaccount.php"><div class="profile_icon"><i class='bx bxs-user'></i><span>My Acount</span></div> </a></li>
+          <li><a class="dropdown-item" href="logout.php"><div class="profile_icon"> <i class='bx bx-log-out'></i><span>Log Out</span></div></a></li>
+         
+        </ul>
+      </div>
+    </nav>
